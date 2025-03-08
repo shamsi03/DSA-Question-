086ac275ff16bedbade447221877a7196dd746a1
shamsi03 <115739797+shamsi03@users.noreply.github.com>
2025-03-08 14:38:22 +0530

    Time: 3 ms (96.76%) | Memory: 45.2 MB (37.05%) - LeetSync

--- 2524-largest-positive-integer-that-exists-with-its-negative/largest-positive-integer-that-exists-with-its-negative.java
+++ 2524-largest-positive-integer-that-exists-with-its-negative/largest-positive-integer-that-exists-with-its-negative.java
@@ -3,14 +3,15 @@ class Solution {
         int hash[] = new int[1001];
         Arrays.fill(hash,Integer.MAX_VALUE);
         int ans = -1;
-        for(int x:nums)
+        for(int it:nums)
         {
-            if(hash[Math.abs(x)]==Integer.MAX_VALUE){
-                hash[Math.abs(x)]=x<0?-1:1;
-            }
-            else{
-                if((x<0 && hash[-x]==1) || (x>0 && hash[x]==-1)) ans=Math.max(ans,Math.abs(x));
-            }
+            if(it<0)
+                hash[-it] = hash[-it]==1?hash[-it]-1:-1;
+            else
+                hash[it] = hash[it]==-1?hash[it]+1:1;
+
+            if(hash[Math.abs(it)] == 0)
+                ans = Math.max(ans,Math.abs(it));
         }
 
         return ans;
