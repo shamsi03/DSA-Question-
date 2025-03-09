45646dd68bc9ba22f134b9b19cd9028358f17133
shamsi03 <115739797+shamsi03@users.noreply.github.com>
2025-03-09 15:48:28 +0530

    Time: 3 ms (33.09%) | Memory: 44.3 MB (6.16%) - LeetSync

--- 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.java
+++ 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.java
@@ -1,14 +1,19 @@
 class Solution {
     public boolean checkIfExist(int[] arr) {
         HashMap<Integer,Integer>m = new HashMap<>();
-        for(int i=0;i<arr.length;i++)
+        int countZero = 0;
+        for(int i=0;i<arr.length;i++){
             m.put(arr[i],i);
+            if(arr[i] == 0) countZero++;
+        }
+
+        if(countZero > 1) return true; //because 0 ko double karne pe 0 tb hi milega jb ek se jyada element 0 ho
         for(int i=0;i<arr.length;i++)
         {
-            if(m.containsKey(2*arr[i]) && m.get(2*arr[i])!=i )  // special condition because of zero 
+            if(m.containsKey(2*arr[i]) && arr[i]!=0 )  // special condition because of zero 
                 return true;
         }
 
-        return false;
+        return false;   
     }
 }
