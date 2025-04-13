73c2b8227ae4712541edafef6d180907239edfdf
shamsi03 <115739797+shamsi03@users.noreply.github.com>
2025-04-13 13:26:45 +0530

    Time: 2 ms (80.66%) | Memory: 43.6 MB (16.78%) - LeetSync

--- 2210-find-target-indices-after-sorting-array/find-target-indices-after-sorting-array.java
+++ 2210-find-target-indices-after-sorting-array/find-target-indices-after-sorting-array.java
@@ -1,27 +1,15 @@
 class Solution {
     public List<Integer> targetIndices(int[] nums, int target) {
 
-        ArrayList<Integer>ans = new ArrayList<>();
+        List<Integer>ans = new ArrayList<>();
         
-        //using selection sort
-        for(int i=0;i<nums.length-1;i++)
-        {
-            int index = i;
-            for(int j=i+1;j<nums.length;j++)
-            {
-                if(nums[index]>nums[j])
-                    index = j;
-            }
-            //swap of most minimum to i'th value
-            int temp = nums[i];
-            nums[i] = nums[index];
-            nums[index] = temp;
-        }
+        Arrays.sort(nums);
 
         for(int i=0;i<nums.length;i++)
         {
             if(nums[i]==target)
                 ans.add(i);
+            else if(nums[i]>target) break;
         }
 
 
