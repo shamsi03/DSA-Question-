9198234dc533725b241ea6ca52a5d9cf89890bf6
shamsi03 <115739797+shamsi03@users.noreply.github.com>
2025-04-18 15:12:34 +0530

    Time: 22 ms (75.10%) | Memory: 61.2 MB (22.31%) - LeetSync

---
+++ 2245-destroying-asteroids/destroying-asteroids.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean asteroidsDestroyed(int mass, int[] asteroids) {
+       Arrays.sort(asteroids);
+    
+       long sum = mass;
+       for(int i=0;i<asteroids.length;i++)
+       {
+            if(sum<asteroids[i])
+                return false;
+
+            sum += asteroids[i];
+       }
+
+       return true;
+    }
+}
