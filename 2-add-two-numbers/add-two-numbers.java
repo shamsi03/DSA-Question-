2a8fa3bf61b2fd8e2990b23446d7bc4a54da5d82
shamsi03 <115739797+shamsi03@users.noreply.github.com>
2025-05-18 13:59:47 +0530

    Time: 1 ms (100.00%) | Memory: 44.4 MB (82.78%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.java
+++ 2-add-two-numbers/add-two-numbers.java
@@ -10,41 +10,42 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(-1);
+        ListNode trav = dummy;
         int carry = 0;
-        //dummy node 
-        ListNode temp=new ListNode(0);
-        //for the calculation
-        ListNode curr=temp;
-        while(l1!=null && l2!=null)
+        while(l1!=null || l2!=null)
         {
-            int sum =  l1.val + l2.val+carry;
-            curr.next=new ListNode(sum%10);
-            curr=curr.next;
-            carry=sum/10; 
-            l1 = l1.next;
-            l2 = l2.next;
-        } 
-        while(l1!=null)
-        {
-            int sum =  l1.val +carry;
-            curr.next=new ListNode(sum%10);
-            curr=curr.next;
-            carry=sum/10; 
-            l1 = l1.next;
-        } 
-        while(l2!=null)
-        {
-            int sum = l2.val+carry;
-            curr.next=new ListNode(sum%10);
-            curr=curr.next;
-            carry=sum/10; 
-            l2 = l2.next;
+            if(l1!=null && l2!=null)
+            {
+                int sum = l1.val + l2.val + carry;
+                trav.next = new ListNode(sum%10);
+                carry = sum/10;
+                trav = trav.next;
+                l1 = l1.next;
+                l2 = l2.next;
+            }
+            else{
+                if(l1!=null)
+                {
+                    int sum = l1.val + carry;
+                    trav.next = new ListNode(sum%10);
+                    carry = sum/10;
+                    trav = trav.next;
+                    l1 = l1.next;
+                }
+                else{
+                    int sum = l2.val + carry;
+                    trav.next = new ListNode(sum%10);
+                    carry = sum/10;
+                    trav = trav.next;
+                    l2 = l2.next;
+                }
+            }
         }
-        //mtlb last wale carry wale me kuch bach jaye tb
-        if(carry>0)  
-            curr.next=new ListNode(carry);
 
-        //refernce return kar diya second node ka , q ki first node to dummy hai uska use nahi kar rhe hai humlog
-        return temp.next;
+        if(carry>0)
+                trav.next = new ListNode(carry);
+
+        return dummy.next;
     }
 }
