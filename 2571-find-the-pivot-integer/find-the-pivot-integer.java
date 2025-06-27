b06c5ce4eac413d549623446de83bd9eff5f0324
shamsi03 <115739797+shamsi03@users.noreply.github.com>
2025-06-27 14:41:30 +0530

    Time: 1 ms (71.18%) | Memory: 40.5 MB (80.25%) - LeetSync

---
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int pivotInteger(int n) {
+        int suffixSum = (n*(n+1))/2;
+        int prefixSum = 0;
+        for(int i=1;i<=n;i++)
+        {
+            prefixSum += i;
+            if(prefixSum==suffixSum)
+                return i;
+
+            suffixSum -= i;
+        }
+
+        return -1;
+    }
+}
