0a3bc385cab503b8256a741ce82d21278df86adf
shamsi03 <115739797+shamsi03@users.noreply.github.com>
2025-06-30 15:02:01 +0530

    Time: 7 ms (86.69%) | Memory: 57.9 MB (39.18%) - LeetSync

---
+++ 1073-number-of-enclaves/number-of-enclaves.java
@@ -0,0 +1,51 @@
+class Solution {
+    public int numEnclaves(int[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+        for(int i=0;i<grid.length;i++)
+        {
+            for(int j=0;j<grid[0].length;j++)
+            {
+                // maine corner walo ko sirf check kara ki kya corner pe 1 hai ya nahi 
+                if(i == 0 || j ==0 || i == n-1 || j == m-1){
+                    if(grid[i][j]==1)
+                    {
+                        dfs(grid,i,j,grid.length,grid[0].length);
+                    }
+                }
+            }
+        }
+        int length = 0;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(grid[i][j]==1)
+                    length++;
+            }
+        }
+
+       
+
+        return length;
+    }
+
+    boolean dfs(int[][]grid,int row,int col,int n,int m)
+    {
+        if(row>=n || col>=m || row<0 || col<0){
+            return false;
+        }
+
+        if(grid[row][col]==0)
+            return true;
+
+        grid[row][col] = 0;
+
+        boolean left = dfs(grid,row,col-1,n,m);
+        boolean right = dfs(grid,row,col+1,n,m);
+        boolean top = dfs(grid,row-1,col,n,m);
+        boolean bottom = dfs(grid,row+1,col,n,m);
+
+        return left && right && top && bottom;
+    }
+}
