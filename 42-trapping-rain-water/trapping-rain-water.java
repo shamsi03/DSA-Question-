4879cf4870812e27bb97454cbcec9f53d072020a
shamsi03 <115739797+shamsi03@users.noreply.github.com>
2025-07-12 13:46:15 +0530

    Time: 1 ms (62.82%) | Memory: 46.2 MB (66.74%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,21 +1,25 @@
 class Solution {
     public int trap(int[] height) {
+        
+        //find the leftMax of each element ->left side me us element ka maximum support kya hai 
         int leftMax[] = new int[height.length];
         for(int i=1;i<height.length;i++)
             leftMax[i] = Math.max(leftMax[i-1],height[i-1]);
-
+        
+        //finding the rightMax of each element -> uske right side me uska maximum support kya  hai 
         int rightMax[] = new int[height.length];
         for(int i=height.length-2;i>=0;i--)
             rightMax[i] = Math.max(rightMax[i+1],height[i+1]);
-        
+
+        //finding the actual trap water
         int trapWater = 0;
         for(int i=1;i<height.length-1;i++)
         {
-            int diff = Math.min(leftMax[i],rightMax[i]) - height[i];
-            if(diff>0)
-                trapWater += diff;
+            int minSupport = Math.min(leftMax[i],rightMax[i]);
+            if(minSupport-height[i]>0)
+                trapWater += minSupport-height[i];
         }
-        
+
         return trapWater;
     }
 }
