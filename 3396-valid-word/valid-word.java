306b00c99ff8a24e0ebdbec05668fb3cf12b7278
shamsi03 <115739797+shamsi03@users.noreply.github.com>
2025-07-15 09:44:37 +0530

    Time: 1 ms (99.12%) | Memory: 42 MB (80.13%) - LeetSync

--- 3396-valid-word/valid-word.java
+++ 3396-valid-word/valid-word.java
@@ -1,23 +1,27 @@
 class Solution {
     public boolean isValid(String word) {
-        if(word.length()<3)
-            return false;
+        
+        //valid word not be less than 3 character
+        if(word.length()<3) return false;
 
-        int vowel = 0, conson = 0;
+
+        int vowel=0,conson=0;
         for(int i=0;i<word.length();i++)
         {
             char ch = word.charAt(i);
-            if(Character.isLetter(ch)){
+            
+            if(Character.isLetter(ch)) //check for letter(upper/lower)
+            {
                 if("aeiou".indexOf(ch)!=-1 || "AEIOU".indexOf(ch)!=-1)
                     vowel++;
                 else
                     conson++;
             }
-            else if(!Character.isDigit(ch))
+            else if(!Character.isDigit(ch)) // check for digit or not
                 return false;
         }
 
-        if(vowel==0 || conson==0) return false;
+        if(vowel==0 || conson ==0) return false;
 
         return true;
     }
