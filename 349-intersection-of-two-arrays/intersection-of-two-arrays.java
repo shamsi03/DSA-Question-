8177ca33fa07d7c9e88584ed286cfadaeaeead35
shamsi03 <115739797+shamsi03@users.noreply.github.com>
2025-07-19 14:25:27 +0530

    Time: 3 ms (50.20%) | Memory: 43.7 MB (22.99%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.java
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.java
@@ -1,22 +1,26 @@
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
-        HashMap<Integer,Integer>m1 = new HashMap<>();
-        HashMap<Integer,Integer>m2 = new HashMap<>();
-        ArrayList<Integer>li = new ArrayList<>();
+        HashSet<Integer>set1 = new HashSet<>();
+        HashSet<Integer>set2 = new HashSet<>();
         for(int it:nums1)
-            m1.put(it,m1.getOrDefault(it,0)+1);
-        for(int it:nums2)
-            m2.put(it,m2.getOrDefault(it,0)+1);
+            set1.add(it);
 
-        for(int it:m2.keySet())
+        for(int it:nums2)
+            set2.add(it);
+
+        HashSet<Integer>set3 = new HashSet<>();
+        for(int it:set1)
         {
-            int target = it;
-            if(m1.containsKey(target))
-                li.add(target);
+            if(set2.contains(it))
+                set3.add(it);
         }
 
-        int ans[] = li.stream().mapToInt(i -> i).toArray();
+        int ans[] = new int[set3.size()];
+        int i=0;
+        for(int it:set3)
+            ans[i++] = it;
 
         return ans;
+
     }
 }
