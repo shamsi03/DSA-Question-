3625ee0f6d5fb0c4fef752df9b3961d63a877f7a
shamsi03 <115739797+shamsi03@users.noreply.github.com>
2025-07-28 21:16:30 +0530

    Time: 1 ms (99.51%) | Memory: 44.1 MB (52.42%) - LeetSync

--- 443-string-compression/string-compression.java
+++ 443-string-compression/string-compression.java
@@ -1,11 +1,11 @@
 class Solution {
     public int compress(char[] chars) {
         
-        int idx = 0;
+        int idx = 0; // for modifying the char array
         for(int i=0;i<chars.length;i++)
         {
             char ch = chars[i];
-            int count = 0;
+            int count = 0; //count the frequency of the each character
 
             while(i<chars.length && ch == chars[i])
             {
@@ -14,19 +14,19 @@ class Solution {
             }
 
             if(count == 1) 
-                chars[idx++] = ch;
+                chars[idx++] = ch; // store only character
             else{
-                chars[idx++] = ch;
+                chars[idx++] = ch; //store character
 
                 String str = String.valueOf(count);
                 for(char ch1 : str.toCharArray())
-                    chars[idx++] = ch1;
+                    chars[idx++] = ch1;  //then store frequency of that 
             }
 
-            i--;
+            i--; // ek baar loop me already i++ kar de rahe hai isliye usko maintain karne k liye 
 
         }
 
-        return idx;
+        return idx;  // yahan tk hi mera compress charcater array hoga 
     }
 }
