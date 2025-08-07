6ab10d3a25d11a68189afa3f6c3bd627ccead3f0
shamsi03 <115739797+shamsi03@users.noreply.github.com>
2025-08-07 20:55:10 +0530

    Time: 2 ms (59.77%) | Memory: 47.5 MB (85.00%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
@@ -1,19 +1,35 @@
 class Solution {
     public int numSubarraysWithSum(int[] nums, int goal) {
         
-        int countSub = 0;
-        for(int i=0;i<nums.length;i++)
-        {
-            int sum = 0;
-            for(int j=i;j<nums.length;j++)
-            {
-                sum += nums[j];
+        //find first number of subarray which sum has less than and equal to goal
 
-                if(sum == goal)
-                    countSub++;
-                else if(sum > goal) 
-                    break;
+        int findSubLessGoal = findCountSub(nums,goal);
+
+        //find the number of subarray which sum have lesser than equal to goal-1 
+
+        int findSubLessGoal_1 = findCountSub(nums,goal-1);
+
+
+        return (findSubLessGoal - findSubLessGoal_1);
+    }
+
+    int findCountSub(int[] nums,int goal)
+    {
+        int left=0,right=0,countSub=0,sum=0;
+        while(right < nums.length)
+        {
+            sum += nums[right];
+
+            while(left < right && sum > goal)
+            {
+                sum -= nums[left];
+                left++;
             }
+
+            if(sum <= goal)
+                countSub += (right-left+1);
+
+            right++;
         }
 
         return countSub;
