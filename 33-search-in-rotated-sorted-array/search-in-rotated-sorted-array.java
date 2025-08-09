f054ef6c7e810061623e02dc30602b94ca885b47
shamsi03 <115739797+shamsi03@users.noreply.github.com>
2025-08-09 17:37:22 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (83.62%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,30 +1,32 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int start=0,end=nums.length-1;
-        while(start<=end)
+        int low = 0, high = nums.length-1;
+
+        while(low <= high)
         {
-            int mid = end - (end-start);
-            if(nums[mid]==target)
+            int mid = low + (high-low)/2;
+
+            if(nums[mid] == target)
                 return mid;
-            else if(nums[0]<=nums[mid]) // means right side sorted array
+            
+            //identify the which half is sorted 
+
+            //for left part sorted
+            if(nums[low] <= nums[mid])
             {
-                //kuch test cases aise honge jinme target and start ki value same hi hogi.
-                // usi test cases ko handle karne k liye equal to lgaye 
-                if(nums[mid]>target && target>=nums[start])
-                    end = mid-1;
+                if(nums[low]<=target && target<=nums[mid])
+                    high = mid-1;
                 else
-                    start = mid+1;
+                    low = mid+1;
             }
-            else{ //means left side sorted 
-                //kuch test cases aise honge jinme target and end ki value same hi hogi.
-                // usi test cases ko handle karne k liye equal to lgaye 
-                if(nums[mid]<target && target<=nums[end])
-                    start = mid+1;
+            else{
+                if(nums[mid]<=target && target<=nums[high])
+                    low = mid+1;
                 else
-                    end = mid-1;
+                    high = mid-1;
             }
         }
 
-        return -1;
+        return -1;//target-element not exists
     }
 }
