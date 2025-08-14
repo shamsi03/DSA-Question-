72cc6971b3bfd1c91c5c47d7a3426fa2d32a8b51
shamsi03 <115739797+shamsi03@users.noreply.github.com>
2025-08-14 20:55:04 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (72.58%) - LeetSync

--- 162-find-peak-element/find-peak-element.java
+++ 162-find-peak-element/find-peak-element.java
@@ -1,9 +1,23 @@
 class Solution {
     public int findPeakElement(int[] nums) {
-        if(nums.length==1) return 0;
-        for(int i=0;i<nums.length;i++)
+        int size = nums.length;
+
+        if(size == 1) return 0;
+        if(nums[0]>nums[1]) return 0;
+        if(nums[size-1]>nums[size-2]) return size-1;
+
+        int low = 1, high = size-2;
+
+        while(low <= high)
         {
-            if((i==0 || nums[i-1]<nums[i]) && (i==nums.length-1 || nums[i]>nums[i+1]))   return i;
+            int mid = low + (high-low)/2;
+
+            if(nums[mid-1]<nums[mid] && nums[mid]>nums[mid+1])
+                return mid;
+            else if(nums[mid-1]<nums[mid]) //mid is on the increasing curve
+                low = mid+1;
+            else // mid is on the decreasing curve or either saturation point
+                high = mid-1;
         }
 
         return -1;
