7cc7c1cf784b0d24ecaff6b3e36291d3da5aebd4
shamsi03 <115739797+shamsi03@users.noreply.github.com>
2025-08-18 14:16:10 +0530

    Time: 0 ms (100.00%) | Memory: 50.1 MB (81.10%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
@@ -1,7 +1,6 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-        
-        //Optimal Approach
+        //Optimal Approach 
 
         int size = nums.length;
 
@@ -9,28 +8,19 @@ class Solution {
         if(nums[0] != nums[1]) return nums[0];
         if(nums[size-2] != nums[size-1]) return nums[size-1];
 
-        int low = 1 , high = size-2;
+        int low = 1 , high = size-1;
+
         while(low <= high)
         {
             int mid = low + (high - low)/2;
 
-            if(nums[mid-1]!=nums[mid]  && nums[mid]!=nums[mid+1]) 
+            if(nums[mid-1]!=nums[mid]  && nums[mid]!=nums[mid+1])
                 return nums[mid];
-            else{
-                if(nums[mid-1]==nums[mid])
-                {
-                    if((mid-1)%2==0 && mid%2!=0) //(even,odd)
-                        low = mid+1;
-                    else
-                        high = mid-1;
-                }
-                else{
-                    if(mid%2!=0 && (mid+1)%2==0 ) //(odd,even)
-                        high = mid-1;
-                    else
-                        low = mid+1;
-                }
-            }
+
+            //I'm on left part eliminate left part
+            if((mid%2==0 && nums[mid] == nums[mid+1]) || (mid%2==1 && nums[mid-1] == nums[mid]))  low = mid+1; 
+            else
+                high = mid-1; //right part eliminate
         }
 
         return -1;
