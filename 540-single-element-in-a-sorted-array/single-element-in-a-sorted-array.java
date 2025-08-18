c29fc2480d8a2a58ec28cbbd931aedbea3264eb1
shamsi03 <115739797+shamsi03@users.noreply.github.com>
2025-08-18 13:58:03 +0530

    Time: 0 ms (100.00%) | Memory: 50 MB (91.84%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
@@ -1,21 +1,35 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
         
-        // Brute force approach
-        int size = nums.length;
-        if(size==1) return nums[0];
+        //Optimal Approach
 
-        for(int i=0;i<size;i++)
+        int size = nums.length;
+
+        if(size == 1) return nums[0];
+        if(nums[0] != nums[1]) return nums[0];
+        if(nums[size-2] != nums[size-1]) return nums[size-1];
+
+        int low = 1 , high = size-2;
+        while(low <= high)
         {
-            if(i==0){//no need to be check the left part -> out of bound 
-                if(nums[i]!=nums[i+1]) return nums[i];
-            }
-            else if(i==size-1){//no need to be check the left part->OutOfBond 
-                if(nums[i-1]!=nums[i]) return nums[i];
-            }
+            int mid = low + (high - low)/2;
+
+            if(nums[mid-1]!=nums[mid]  && nums[mid]!=nums[mid+1]) 
+                return nums[mid];
             else{
-                if(nums[i-1]!=nums[i] && nums[i]!=nums[i+1])
-                    return nums[i];
+                if(nums[mid-1]==nums[mid])
+                {
+                    if((mid-1)%2==0 && mid%2!=0) //(even,odd)
+                        low = mid+1;
+                    else
+                        high = mid-1;
+                }
+                else{
+                    if(mid%2!=0 && (mid+1)%2==0 ) //(odd,even)
+                        high = mid-1;
+                    else
+                        low = mid+1;
+                }
             }
         }
 
