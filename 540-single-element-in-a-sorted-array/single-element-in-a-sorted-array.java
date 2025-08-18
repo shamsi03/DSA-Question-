6962698bf4ba1ca5b25d2040fb1d5f54cafd0318
shamsi03 <115739797+shamsi03@users.noreply.github.com>
2025-08-18 13:49:01 +0530

    Time: 2 ms (9.95%) | Memory: 50.5 MB (40.96%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
@@ -1,9 +1,24 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-        int ans = 0;
-        for(int it:nums)
-            ans ^= it;
         
-        return ans;
+        // Brute force approach
+        int size = nums.length;
+        if(size==1) return nums[0];
+
+        for(int i=0;i<size;i++)
+        {
+            if(i==0){//no need to be check the left part -> out of bound 
+                if(nums[i]!=nums[i+1]) return nums[i];
+            }
+            else if(i==size-1){//no need to be check the left part->OutOfBond 
+                if(nums[i-1]!=nums[i]) return nums[i];
+            }
+            else{
+                if(nums[i-1]!=nums[i] && nums[i]!=nums[i+1])
+                    return nums[i];
+            }
+        }
+
+        return -1;
     }
 }
