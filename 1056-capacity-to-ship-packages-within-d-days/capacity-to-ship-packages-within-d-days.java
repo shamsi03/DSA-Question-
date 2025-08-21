4d82deb86306730572bc7f8e744ee65477387da8
shamsi03 <115739797+shamsi03@users.noreply.github.com>
2025-08-21 18:38:58 +0530

    Time: 11 ms (77.22%) | Memory: 47.1 MB (85.01%) - LeetSync

--- 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.java
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.java
@@ -1,20 +1,21 @@
 class Solution {
     public int shipWithinDays(int[] weights, int days) {
-        
-        //find the sum of all elements -> for deciding my answer space
-        int sum = 0;
-        for(int it:weights)
+         //find the sum of all elements and max-element also -> for deciding my answer space
+        int sum = 0 , maxElement=-1;
+        for(int it:weights){
             sum += it;
+            maxElement = Math.max(it,maxElement);
+        }
 
         int ans = 0;
-        int low = 1 , high = sum; //this is my answer space -> now apply BS on this
+        int low = maxElement , high = sum; //this is my answer space -> now apply BS on this
         while(low <= high)
         {
             int mid = low + (high-low)/2;
 
             int totalDays = countDays(weights,mid);
 
-            if(totalDays!=-1 && totalDays <= days)
+            if(totalDays <= days)
             {
                 ans = mid;
                 high = mid - 1; 
@@ -26,13 +27,11 @@ class Solution {
         return ans;
     }
 
-    int countDays(int[] nums, int capacity)
+    int countDays(int[] nums,int capacity)
     {
-        int sum = 0 , countDay = 1;
+        int countDay = 1, sum = 0;
         for(int it:nums)
         {
-            if(it > capacity) return -1;
-
             sum += it;
             if(sum > capacity)
             {
