69ef16934c50667dcbdc6ea154ddb5bd11b02695
shamsi03 <115739797+shamsi03@users.noreply.github.com>
2025-08-23 11:41:12 +0530

    Time: 22 ms (28.26%) | Memory: 55.6 MB (98.48%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.java
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.java
@@ -1,44 +1,53 @@
 class Solution {
+    public int minDays(int[] bloomDay, int m, int k) {
+        //Optimal approach -> apply binary search on the search space
 
-    public boolean isPossible(int day,int[] nums,int m , int k)
-    {
-        int adj = 0, makeBouq = 0;
-        for(int i=0;i<nums.length;i++)
+        //find the min and max element -> for deciding the answer space
+        int minElement = Integer.MAX_VALUE , maxElement = -1;
+        for(int it: bloomDay)
         {
-            if(nums[i]<=day)
-            {
-                adj++;
-                if(adj == k)
-                {
-                    makeBouq++;
-                    adj = 0;
-                }
-            }
-            else
-                adj = 0;
+            maxElement = Math.max(it,maxElement);
+            minElement = Math.min(it,minElement);
         }
 
-        return makeBouq>=m;
-    }
-    public int minDays(int[] bloomDay, int m, int k) {
-        int max = -1;
-        for(int it:bloomDay)
-            max = Math.max(max,it);
-
-        int start=0,end=max,ans=-1;
-        while(start<=end)
+        //binary search apply 
+        int ans = -1;
+        int low = minElement , high = maxElement; //this is my answer space 
+        while(low <= high)
         {
-            int mid = end - (end-start)/2;
+            int mid = low + (high-low)/2;
 
-            if(isPossible(mid,bloomDay,m,k))
+            int totalBouquets = findPossibleBouquets(bloomDay,mid,k);
+
+            if(totalBouquets >= m)
             {
                 ans = mid;
-                end = mid-1;
+                high = mid-1; // for looking the minimum 
             }
             else
-                start = mid+1;
+                low = mid + 1; //looking for the maximum
         }
 
         return ans;
+    }
+
+
+    int findPossibleBouquets(int[] bloomDay,int day,int k)
+    {
+        int countAdjacent = 0 , totalBouquets = 0;
+        for(int it:bloomDay)
+        {
+            //adjacent flower count , if flower bloom on that day
+            if(it <= day)
+                countAdjacent++; 
+            else{ // flower not blooom.
+                totalBouquets += (countAdjacent/k); //kitne bouquets bana skte h.
+                countAdjacent = 0;
+            }
+        }
+        //last me ek baar phir se find kar liya kitne bouquets ban skte hai
+        totalBouquets += (countAdjacent/k);  
+
+        return totalBouquets;
     }
 }
