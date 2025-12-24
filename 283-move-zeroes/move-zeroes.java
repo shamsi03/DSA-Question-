eb206445ce74a7bcf016395c0e7e0a91e68b284d
Zafrul Islam Shamsi <115739797+shamsi03@users.noreply.github.com>
2025-12-24 16:13:02 +0530

    Time: 3 ms (20.29%) | Memory: 48 MB (12.59%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,22 +1,18 @@
 class Solution {
     public void moveZeroes(int[] nums) {
         int i=0,j=0;
-        while(j<nums.length)
-        {
-            if(nums[i]==0 && nums[j]==0) j++;
-            else if(nums[i]==0 && nums[j]!=0)
-            {
+        while(j<nums.length){
+            if(nums[i]!=0 && nums[j]!=0){
+                i++;
+                j++;
+            }
+            else if(nums[i]==0 && nums[j]!=0){
                 int temp = nums[i];
                 nums[i] = nums[j];
                 nums[j] = temp;
                 i++;
-                j++;
             }
-            else
-            {
-                i++;
-                j++;
-            }
+            else if(nums[i]==0 && nums[j]==0) j++;
         }
     }
 }
