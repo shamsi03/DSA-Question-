76e678bbe8e612ab3aefcde4300676ccc9575d52
Zafrul Islam Shamsi <115739797+shamsi03@users.noreply.github.com>
2026-07-18 12:50:50 +0530

    Time: 7 ms (6.51%) | Memory: 45.7 MB (11.44%) - LeetSync

--- 2106-find-greatest-common-divisor-of-array/find-greatest-common-divisor-of-array.java
+++ 2106-find-greatest-common-divisor-of-array/find-greatest-common-divisor-of-array.java
@@ -5,8 +5,16 @@ class Solution {
     }
 
     int GCD(int num1,int num2){
-        if(num2 == 0) return num1;
+        // if(num2 == 0) return num1;
 
-        return GCD(num2,num1%num2);
+        // return GCD(num2,num1%num2);
+
+        while(num2 != 0){
+            int temp = num2;
+            num2 = num1%num2;
+            num1 = temp;
+        }
+
+        return num1;
     }
 }
